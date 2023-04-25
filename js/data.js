969ed09aef0bad1acfabd752c3bd40bf6691c596
Cindy Le <cindee.le@gmail.com>
2023-04-25 01:44:04 +0000

    html and css with layout #1

--- js/data.js
+++ js/data.js
@@ -1,1 +1,18 @@
 /* exported data */
+var data = {
+  view: 'entry-form',
+  entries: [],
+  editing: null,
+  nextEntryId: 1
+};
+
+window.addEventListener('beforeunload', function (event) {
+  const codeJournalJSON = JSON.stringify(data);
+  localStorage.setItem('local-storage', codeJournalJSON);
+});
+
+const previousJournalEntries = localStorage.getItem('local-storage');
+
+if (previousJournalEntries !== 'null') {
+  data = JSON.parse(previousJournalEntries);
+}
