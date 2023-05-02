673f48dd6d6992fbba4a285139bd8c6c1759df55
Cindy Le <cindee.le@gmail.com>
2023-05-02 05:52:49 +0000

    add delete buttons to all pokemon in team view. add event listener to delete buttons. when delete button is clicked, modal appears asking to confirm or cancel. #5

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 const searchButton = document.getElementById('search');
 let searchedPokemon = data.searchedPokemon;
-let searchPokemonName = 'pikachu';
+let searchPokemonName = '';
 let flavorText = '';
+let deletePokemonId = null;
 
 searchButton.addEventListener('click', searchForPokemon);
 if (data.pageView === 'team') {
   changeViewToTeam();
 }
-if (data.pageView === 'pokemon') {
+if (data.pageView === 'pokemon' && searchedPokemon) {
   changeViewToSearchedPokemon();
 }
+
 function changeViewToSearchedPokemon() {
   data.pageView = 'pokemon';
   document.querySelector('.home').classList.add('display-none');
@@ -110,6 +112,7 @@ function changeViewToTeam() {
     const pokemon = data.team[index];
     const div = document.createElement('div');
     div.className = 'col individual';
+    div.setAttribute('target-id', pokemon.id);
     div.addEventListener('click', function () {
       searchedPokemon = data.team[index];
       resetSearchedPokemon();
@@ -119,7 +122,23 @@ function changeViewToTeam() {
     image.setAttribute('src', pokemon.sprites.front_default);
     div.appendChild(image);
 
+    const deleteBtn = document.createElement('button');
+    deleteBtn.setAttribute('target-id', pokemon.id);
+    deleteBtn.className = 'delete-btn';
+    deleteBtn.innerText = 'remove';
+    deleteBtn.classList.add('btn');
+    deleteBtn.classList.add('btn-primary');
+    deleteBtn.setAttribute('type', 'button');
+    deleteBtn.setAttribute('data-bs-toggle', 'modal');
+    deleteBtn.setAttribute('data-bs-target', '#modal');
+
+    div.appendChild(deleteBtn);
+
     document.querySelector('.team-container').appendChild(div);
+
+    deleteBtn.addEventListener('click', function (event) {
+      deletePokemonId = event.target.getAttribute('target-id');
+    });
   }
 
 }
@@ -161,3 +180,22 @@ togglePic.addEventListener('click', function () {
     document.querySelector('.searched-pokemon-image').setAttribute('src', frontDefaultSprite);
   }
 });
+
+function deleteTeam(event) {
+
+  let index = null;
+  for (let i = 0; i < data.team.length; i++) {
+
+    if (parseInt(deletePokemonId) === parseInt(data.team[i].id)) {
+      index = i;
+    }
+  }
+  if (index) {
+    data.team.splice(index, 1);
+  }
+  // const removeDiv = document.querySelector()
+  // div.remove();
+
+}
+
+deleteTeam();
